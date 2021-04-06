80d36c693ed3989ccffe38f7e61fa3c9c8ccc1f9
Кристина Фоменко <kris.inferno@gmail.com>
2021-04-06 15:36:12 +0300

    Initial commit

---
+++ controllers.py
@@ -0,0 +1,78 @@
+from views import render_template
+
+from models import User, Phone
+
+
+def default_controller(data=None, cls=True) :
+    render_template(context={}, template="default.jinja2", cls=cls)
+    return (input(), None)
+
+
+def exit_controller(data=None, cls=True) :
+    render_template(context={}, template="exit.jinja2", cls=cls)
+    exit()
+
+
+def all_users_controller(data=None, cls=True) :
+    users = User.all()
+    render_template(context={'users' : users}, template="all_users.jinja2", cls=cls)
+    input("Продолжить?")
+    return 'main', None
+
+
+def add_user_controller(data=None, cls=True) :
+    render_template(context={}, template="add_user.jinja2", cls=cls)
+    username = input()
+    user = User.add(username)
+    return 21, user
+
+
+def add_user_controller(data=None, cls=True) :
+    render_template(context={}, template="add_user.jinja2", cls=cls)
+    username = input()
+    user = User.add(username)
+    return 21, user
+
+
+def add_phone_controller(user, cls=True) :
+    render_template(context={}, template="add_phone.jinja2", cls=cls)
+    phone_number = input()
+    phone = Phone.add(phone_number, user)
+    return 212, user
+
+
+def find_user_controller(data=None, cls=True) :
+    render_template(context={}, template="find_user.jinja2", cls=cls)
+    username = input()
+    user = User.add(username)
+    return 11, username
+
+
+def show_number_controller(username, cls=True) :
+    render_template(context={}, template="show_number.jinja2", cls=cls)
+    user = User.add(username)
+    return 12, user
+
+
+def add_more_controller(user, cls=True) :
+    render_template(context={}, template="add_more.jinja2", cls=cls)
+    answer = input()
+    if answer == 'Y' :
+        return 21, user
+    return 51, user
+
+
+def get_controller(state) :
+    return controllers_dict.get(state, default_controller)
+
+
+controllers_dict = {
+    '0' : exit_controller,
+    '1' : all_users_controller,
+    '2' : add_user_controller,
+    '3' : find_user_controller,
+    21 : add_phone_controller,
+    212 : add_more_controller,
+    11 : show_number_controller,
+    12 : find_user_controller
+}
---
+++ views.py
@@ -0,0 +1,19 @@
+import os
+from jinja2 import Environment, PackageLoader, select_autoescape
+env = Environment(
+    loader=PackageLoader(__name__, 'templates'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+def screen_cleaner(flag):
+    if flag:
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def render_template(context=None, template="default.jinja2", cls=True):
+
+    if not context:
+        context = {}
+    screen_cleaner(cls)
+    template = env.get_template(template)
+    print(template.render(**context))
